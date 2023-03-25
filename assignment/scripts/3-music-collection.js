137819733860d05d498712e88e5bd64d141ce2fc
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-24 19:33:05 -0700

    Added .tracks to the second album in the collections array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,15 +4,16 @@ console.log('***** Music Collection *****')
 // Create a variable collection that starts as an empty array.
 let collection = [];
 
-console.log('--- addToCollection ---');
+console.log('\n--- addToCollection ---\n\n');
 // Add a function named addToCollection. This function should:
     // Take in the album's title, artist, yearPublished as input parameters
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished, tracks){
     // Create a new object having the above properties
     let newAlbum = {
-        albumTitle : title,
-        artistName : artist,
-        albumPublished : yearPublished
+        title : title,
+        artist : artist,
+        yearPublished : yearPublished,
+        tracks: tracks
     }
     // Add the new object to the end of the collection array
     collection.push(newAlbum)
@@ -24,18 +25,24 @@ function addToCollection(title, artist, yearPublished){
 // Test the addToCollection function:
     // Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
     // Console.log each album as added using the returned value.
-    // After all are added, console.log the collection array.
-console.log('Adding new album to my collection:', addToCollection('Camp', 'Childish Gambino', 2011));
-console.log('Adding new album to my collection:', addToCollection('Awaken, My Love!', 'Childish Gambino', 2016));
+    
+console.log('Adding new album to my collection:', addToCollection('Camp', 'Childish Gambino', 2011, [{name: 'Outside', duration: '4m 30s'}, {name: 'Fire Fly', duration: '3m 23s'}, {name: 'Bonfire', duration: '3m 13s'}, {name: 'All the Shine', duration: '5m 46s'}, {name: 'Letter Home', duration: '1m 44s'}, {name: 'Heartbeat', duration: '4m 30s'}, {name: 'Backpackers', duration: '3m 16s'}, {name: 'Les', duration: '5m 17s'}, {name: 'Hold You Down', duration: '4m 53s'}, {name: 'Kids', duration: '4m 58s'}, {name: 'You See Me', duration: '3m 15s'}, {name: 'Sunrise', duration: '3m 40s'}, {name: 'That Power', duration: '7m 42s'}]));
+
+console.log('Adding new album to my collection:', addToCollection('Awaken, My Love!', 'Childish Gambino', 2016, [{name: 'Me and Your Mama', duration: '6m 19s'}, {name: 'Have Some Love', duration: '3m 44s'}, {name: 'Boogieman', duration: '3m 36s'}, {name: 'Zombies', duration: '4m 41s'}, {name: 'Riot', duration: '2m 05s'}, {name: 'Redbone', duration: '5m 26s'}, {name: 'California', duration: '2m 45s'}, {name: 'Terrified', duration: '4m 15s'}, {name: 'Baby Boy', duration: '6m 22s'}, {name: 'The Night Me and Your Mama Met', duration: '3m 34s'}, {name: 'Stand Tall', duration: '6m 10s'}]));
+
 console.log('Adding new album to my collection:', addToCollection('Mr. Morale & The Big Steppers', 'Kendrick Lamar', 2022));
+
 console.log('Adding new album to my collection:', addToCollection('Castor, The Twin', 'Dessa', 2011));
+
 console.log('Adding new album to my collection:', addToCollection('Cleopatra (Deluxe)', 'The Lumineers', 2016));
+
 console.log('Adding new album to my collection:', addToCollection('Dark Eyes', 'Half Moon Run', 2013));
 
+// After all are added, console.log the collection array.
 console.log('My album collection now includes:', collection);
 
 
-console.log('--- showCollection ---');
+console.log('\n--- showCollection ---\n\n');
 // Add a function named showCollection. This function should:
     // Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
 function showCollection(array) {
@@ -43,7 +50,7 @@ function showCollection(array) {
     console.log('The following', array.length, 'albums are in my collection');
     // Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
     for (let i = 0; i < array.length; i++) {
-        console.log(`${array[i].albumTitle} by ${array[i].artistName}, published in ${array[i].albumPublished}.`);
+        console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}.`);
     }
     return `End showCollection`
 }
@@ -53,15 +60,15 @@ function showCollection(array) {
 console.log(showCollection(collection));
 
 
-console.log('--- findByArtist ---');
+console.log('\n--- findByArtist ---\n\n');
 // Add a function named findByArtist. This function should:
     // Take in artist (a string) parameter
-function findByArtist(artist){
+function findByArtist(artist_to_find){
     // Create an array to hold any results, empty to start
     let albums_by_artist = []
     // Loop through the collection and add any objects with a matching artist to the array.
     for (let i = 0; i < collection.length; i++){
-        if (collection[i].artistName === artist) {
+        if (collection[i].artist === artist_to_find) {
             albums_by_artist.push(collection[i])
         }
     }
@@ -80,7 +87,7 @@ console.log('Is Childish Gambino in my currenct collection?', findByArtist('Chil
 
 
 // Stretch goals
-console.log('--- Stretch Goals ---');
+console.log('\n--- Stretch Goals ---\n\n');
 // Create a function called search. This function should:
     // Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
         // { artist: 'Ray Charles', year: 1957 }
@@ -91,7 +98,7 @@ console.log('Adding new album to my collection:', addToCollection('Ray Charles',
 function search(collectionToSearch, artist_name, albumYear){
     let albums_matching_parameters = []
     for (let i = 0; i < collectionToSearch.length; i++) {
-        if (collectionToSearch[i].artistName === artist_name && collectionToSearch[i].albumPublished === albumYear) {
+        if (collectionToSearch[i].artist === artist_name && collectionToSearch[i].yearPublished === albumYear) {
             albums_matching_parameters.push(collectionToSearch[i])
         }
     }
@@ -129,26 +136,34 @@ console.log('Expected 2 albums. --->', search(collection, 'Ray Charles', 1957));
 
 
 
-
-// collection[0] = ('Camp', 'Childish Gambino', 2011));
-collection[0].tracks=[{name: 'Outside', duration: '4m 30s'}, {name: 'Fire Fly', duration: '3m 23s'}, {name: 'Bonfire', duration: '3m 13s'}, {name: 'All the Shine', duration: '5m 46s'}, {name: 'Letter Home', duration: '1m 44s'}, {name: 'Heartbeat', duration: '4m 30s'}, {name: 'Backpackers', duration: '3m 16s'}, {name: 'Les', duration: '5m 17s'}, {name: 'Hold You Down', duration: '4m 53s'}, {name: 'Kids', duration: '4m 58s'}, {name: 'You See Me', duration: '3m 15s'}, {name: 'Sunrise', duration: '3m 40s'}, {name: 'That Power', duration: '7m 42s'}];
+console.log(collection[0]);
 
 // collection[1] = ('Awaken, My Love!', 'Childish Gambino', 2016));
-collection[1].tracks=[{name: '', duration: ''}]
+
+console.log(collection[1]);
 
 // collection[2] = ('Mr. Morale & The Big Steppers', 'Kendrick Lamar', 2022));
 collection[2].tracks=[{name: '', duration: ''}]
 
+console.log(collection[2]);
+
 // collection[3] = ('Castor, The Twin', 'Dessa', 2011));
 collection[3].tracks=[{name: '', duration: ''}]
+
+console.log(collection[3]);
 
 // collection[4] = ('Cleopatra (Deluxe)', 'The Lumineers', 2016));
 collection[4].tracks=[{name: '', duration: ''}]
 
+console.log(collection[4]);
+
 // collection[5] = ('Dark Eyes', 'Half Moon Run', 2013));
 collection[5].tracks=[{name: '', duration: ''}]
+
+console.log(collection[5]);
 
 // collection[6] = ('Ray Charles', 'Ray Charles', 1957));
 collection[6].tracks=[{name: '', duration: ''}]
 
-console.log(collection[0]);
+console.log(collection[6]);
+
